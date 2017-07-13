047d4776daab2ffc84eaa64e487636f81f76e42f
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-07-13 20:03:04 +0900

    Add Wpf and .Net Standard projects

---
+++ HttpClientSample.Core/Models/HttpGetService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientSample.Core.Models
+{
+    public class HttpGetService
+    {
+        HttpClient _client=new HttpClient();
+        public async Task<string> GetTextFromUrl(string url)
+        {
+            return await _client.GetStringAsync(url);
+        }
+    }
+}
---
+++ HttpClientSample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using HttpClientSample.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HttpClientSample.Wpf
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnGet_OnClick(object sender, RoutedEventArgs e)
+        {
+            var ser = new HttpGetService();
+            var buf = await ser.GetTextFromUrl(edtUrl.Text);
+            MessageBox.Show($"Content={buf.Substring(0,200)}");
+        }
+    }
+}
