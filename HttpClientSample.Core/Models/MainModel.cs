99fc01e378606fdb2efb6792715b0c18c9aaa4a4
Storu Fujimori <satoru.fujimori@gmail.com>
2017-08-02 16:15:27 +0900

    Update to .Net Standard 2.0

---
+++ HttpClientSample.Core/Models/MainModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HttpClientSample.Shared.Models
+{
+    public class MainModel : BindableBase
+    {
+		private string dataDir;
+		public string DataDir
+		{
+			get { return this.dataDir; }
+			set { this.SetProperty(ref this.dataDir, value); }
+		}
+
+		private string url;
+		public string Url
+		{
+			get { return this.url; }
+			set { this.SetProperty(ref this.url, value); }
+		}
+    }
+
+	public class BindableBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+		{
+			if (Equals(field, value)) { return false; }
+			field = value;
+			var h = this.PropertyChanged;
+			if (h != null) { h(this, new PropertyChangedEventArgs(propertyName)); }
+			return true;
+		}
+	}
+}
---
+++ HttpClientSample.Core/ViewModels/ViewModel.cs
@@ -0,0 +1,66 @@
+﻿﻿using System;
+using Reactive.Bindings;
+using System.Reactive.Linq;
+using System.Windows;
+using HttpClientSample.Shared.Models;
+using Reactive.Bindings.Extensions;
+#if __WPF__
+#endif
+#if __MAC__
+using AppKit;
+#endif
+
+namespace HttpClientSample.Shared.ViewModels
+{
+    public class ViewModel
+    {
+		public ReactiveProperty<string> Url { get; }
+		public ReactiveProperty<string> DataDir { get; }
+		public ReactiveCommand ExportFileCommand { get; }
+        MainModel Model { get; } = new MainModel();
+
+        public ViewModel()
+        {
+            Url = this.Model
+            	.ObserveProperty(x => x.Url)
+            	.ToReactiveProperty();
+            DataDir = this.Model
+                .ObserveProperty(x => x.DataDir)
+                .ToReactiveProperty();
+
+            //Url = this.Model
+            //          .ToReactivePropertyAsSynchronized(x => x.Url);
+            //DataDir = this.Model
+                          //.ToReactivePropertyAsSynchronized(x => x.DataDir);
+
+			// URLに文字列があるかつディレクトリがあるなら実行可能
+			ExportFileCommand = DataDir
+                .CombineLatest(Url, (dataDir,url)=> !string.IsNullOrEmpty(url)
+                               && !string.IsNullOrEmpty(dataDir)
+                               && System.IO.Directory.Exists(dataDir))
+                .ToReactiveCommand();
+
+			ExportFileCommand
+                .Subscribe(() => SaveFileFromUrl());
+        }
+
+        // URLの内容をファイルに保存する
+        void SaveFileFromUrl()
+        {
+            var ser = new HttpWebRequestService();
+            var savePath = System.IO.Path.Combine(DataDir.Value, "yahoo.htm");
+            ser.SaveFileFromUrl(Url.Value, savePath);
+            var msg = $"完了しました\n{savePath}";
+#if __WPF__
+            MessageBox.Show(msg);
+#endif
+#if __MAC__
+            using (var alert = new NSAlert())
+            {
+                alert.MessageText = msg;
+                alert.RunSheetModal(null);
+            }
+#endif
+        }
+    }
+}
